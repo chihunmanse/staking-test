5c1a6125e8f988540b81a8b2693ea1cd6f12eca5
chihunmanse <chihunmanse@gmail.com>
2022-10-03 21:30:48 +0900

    MasterChef

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,10 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-truffle5";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: { compilers: [{ version: "0.8.0" }, { version: "0.6.12" }] },
 };
 
 export default config;
---
+++ test/masterChef.ts
@@ -0,0 +1,37 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { Contract } from "ethers";
+import { ethers, expect, network } from "hardhat";
+
+describe("Master Chef", () => {
+  const VERSION = 1;
+
+  let admin: SignerWithAddress, notAdmin: SignerWithAddress;
+  let masterChef: Contract, sushiToken: Contract;
+
+  before(async () => {
+    [admin, notAdmin] = await ethers.getSigners();
+    console.log("Deploying contracts with the account: " + admin.address);
+
+    const SushiToken = await ethers.getContractFactory("SushiToken");
+    sushiToken = await SushiToken.deploy();
+    await sushiToken.deployed();
+
+    const blockNumber = await ethers.provider.getBlockNumber();
+    console.log("Block Number: " + blockNumber);
+
+    const MasterChef = await ethers.getContractFactory("MasterChef");
+    masterChef = await MasterChef.deploy(
+      sushiToken.address,
+      admin.address,
+      10,
+      blockNumber,
+      blockNumber + 10000
+    );
+  });
+
+  describe("", () => {
+    it("", async () => {
+      console.log(masterChef.address);
+    });
+  });
+});
