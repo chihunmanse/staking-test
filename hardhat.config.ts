b22f29ad0bf62f134799ed9336b54cf9bda76012
chihunmanse <chihunmanse@gmail.com>
2022-10-16 23:55:06 +0900

    uniswap

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,7 +4,14 @@ import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-truffle5";
 
 const config: HardhatUserConfig = {
-  solidity: { compilers: [{ version: "0.8.0" }, { version: "0.6.12" }] },
+  solidity: {
+    compilers: [
+      { version: "0.8.0" },
+      { version: "0.6.12" },
+      { version: "0.5.16" },
+      { version: "0.6.6" },
+    ],
+  },
 };
 
 export default config;
